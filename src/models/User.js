34d1ab1fda83998907b4b375f8c4e4bd52c7c259
R4vage <julianmarc@gmail.com>
2022-07-09 19:33:22 -0300

    Modelo de usuario creado

--- src/models/User.js
+++ src/models/User.js
@@ -2,9 +2,48 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
     {
-
+      // aca va toda la estructura de la tabla usuario
+      name: {
+        type: String,
+        required: true, // hace que este campo sea obligatorio
+        trim: true // te quita los espacios de adelante y de atras
+      },
+      lastName: {
+        type: String,
+        required: true, // hace que este campo sea obligatorio
+        trim: true // te quita los espacios de adelante y de atras
+      },
+      password: {
+        type: String,
+        required: true, 
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true, 
+        trim: true,
+        unique: true
+      },
+      token: {
+        type: String
+      },
+      birthday: {
+        type: Date
+      },
+      isDeleted: {
+        type: Boolean,
+        default: false
+      },
+      confirmed: {
+        // la idea es mandarle un email para confirmar su cuenta y ahi pasa a ser true
+        type: Boolean,
+        default: false
+      }
+    },
+    {
+      timestamps: true // crea 2 columnas, una de creado y otra de actualizado
     }
-)
+  );
 
 const User = mongoose.model("User", userSchema);
 export default User;
