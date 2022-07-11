37ed266c95f702732b69b916006847cf75d5a40e
R4vage <julianmarc@gmail.com>
2022-07-11 13:21:56 -0300

    Rename y refactor de modelos

---
+++ src/models/CategoryList.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const categoryListSchema = mongoose.Schema({
+    categories: [
+        {
+            name: {
+                type: String,
+                required: true, // hace que este campo sea obligatorio
+                trim: true // te quita los espacios de adelante y de atras
+            },
+
+            color: {
+                type: String,
+                required: true,
+                trim: true 
+            }
+        },
+    ],
+
+    userID: {
+        type: String,
+        required: true,
+        ref: "User"
+    }
+        
+        
+  },
+)
+
+const CategoryList = mongoose.model("CategoryList", categoryListSchema);
+export default CategoryList;
---
+++ src/models/ExpenseList.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const expenseListSchema = mongoose.Schema({
+    expenses:[{
+        name: {
+            type: String,
+            required: true, // hace que este campo sea obligatorio
+            trim: true // te quita los espacios de adelante y de atras
+        },
+
+        value: {
+            type: Number, //Habria que ver como reacciona a los decimales. Round a dos decimales por ahora, y si tenemos tiempo ver de hacer algo para mas precisión.
+            required: true,
+            trim: true 
+        },
+
+        date: {
+            type: Date,
+            required: true
+        }, // La fecha en la cual ocurrio el gasto. Por ahora el dia nomás, habria que ver si le agregamos
+
+        categoryID: {
+            type: String,
+            required: true,
+            ref: "CategoryList"
+        }
+    }],
+
+    userID: {
+        type: String,
+        required: true,
+        ref: "User"
+    }
+
+  },
+)
+
+const ExpenseList = mongoose.model("ExpenseList", expenseListSchema);
+export default ExpenseList;
