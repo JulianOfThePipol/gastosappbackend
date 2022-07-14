41ec408860ffd209455648586bea3e4bec2a4f4b
R4vage <julianmarc@gmail.com>
2022-07-14 11:53:26 -0300

    Rutas para ver lista y agregar gastos

--- src/routes/expenseList.routes.js
+++ src/routes/expenseList.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { addExpense, getExpenseList } from "../controllers/expenseLists.controllers";
 
 const expenseListRouter = express.Router();
+
+categoryListRouter.get("/", checkAuth, getExpenseList)
+categoryListRouter.post("/", checkAuth, addExpense)
+
+
 
 export  default expenseListRouter
