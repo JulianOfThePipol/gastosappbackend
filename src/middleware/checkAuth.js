b3c8a752624c1eab9bdad6a51890d9c8cd56cbf4
R4vage <julianmarc@gmail.com>
2022-07-30 15:15:29 -0300

    smallFix logged=notLogged

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -9,10 +9,10 @@ export default async function checkAuth (req, res, next) {
             token = req.headers.authorization.split(" ")[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.id).select("-password -tokenConfirm -tokenForgot -isDeleted -confirmed -birthday -createdAt -updatedAt -__v");
-            if (!req.user){return res.status(404).json({msg: "Habemus cagada, el usuario tiene un token válido, pero el id dentro del token no está en la base de datos", logged:true, error:true} )}//Este error es un caso extremo
+            if (!req.user){return res.status(404).json({msg: "Habemus cagada, el usuario tiene un token válido, pero el id dentro del token no está en la base de datos", notLogged:true, error:true} )}//Este error es un caso extremo
             return next()
         } catch (error) {
-            return res.status(401).json({msg: `Token erroneo, o fallo el decode. Mensaje de error: ${error}`, logged:true, error:true})
+            return res.status(401).json({msg: `Token erroneo, o fallo el decode. Mensaje de error: ${error}`, notLogged:true, error:true})
         }
     } else {
         res.status(401).json({msj: "El usuario no está logueado, o fallo el envió de su token", notLogged:true, error:true})
